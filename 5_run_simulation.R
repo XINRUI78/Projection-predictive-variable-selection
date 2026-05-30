4aac50419dc82f9122312d5305fe32c493a4816e
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-05-30 19:05:46 +0100

    Update 5_run_simulation.R

--- 5_run_simulation.R
+++ 5_run_simulation.R
@@ -4,11 +4,11 @@ source("3_performance_measures.R")
 source("4_projpred_functions.R")
 
 # Register one parallel backend for cv_varsel(parallel = TRUE)
-cl <- parallel::makeCluster(9)
+cl <- parallel::makeCluster(4)
 doParallel::registerDoParallel(cl)
 
 proj_results <- foreach(
-  i = 1:9,
+  i = 1:4,
   .combine = rbind,
   .packages = c("rstanarm", "projpred", "pROC", "dplyr", "mvtnorm", "speedglm"),
   .export = c("run_projpred_one", "run_one_prior", "generate_ss", "measures")
