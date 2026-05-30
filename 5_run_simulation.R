a4ff303a46f941a0d3d023e64706e5ba643463de
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-05-30 17:37:23 +0100

    Update 5_run_simulation.R

--- 5_run_simulation.R
+++ 5_run_simulation.R
@@ -1,17 +1,20 @@
-# Register one parallel backend for cv_varsel(parallel = TRUE)
 source("1_sim_parameters.R")
 source("2_data_generation.R")
 source("3_performance_measures.R")
 source("4_projpred_functions.R")
-cl <- parallel::makeCluster(4)
+
+# Register one parallel backend for cv_varsel(parallel = TRUE)
+cl <- parallel::makeCluster(9)
 doParallel::registerDoParallel(cl)
 
-proj_results <- do.call(
-  rbind,
-  lapply(1:10, function(i) {
-    run_projpred_one(i, ndev, n.para, beta0, beta, nval)
-  })
-)
+proj_results <- foreach(
+  i = 1:9,
+  .combine = rbind,
+  .packages = c("rstanarm", "projpred", "pROC", "dplyr"),
+  .export = c("run_projpred_one", "run_one_prior", "generate_ss", "measures")
+) %dopar% {
+  run_projpred_one(i, ndev, n.para, beta0, beta, nval)
+}
 
 parallel::stopCluster(cl)
 foreach::registerDoSEQ()
