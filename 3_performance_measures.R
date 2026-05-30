c374a2d0cb56bf4a4c9bdb75b910bb695d938d91
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-05-30 17:11:12 +0100

    Update 3_performance_measures.R

--- 3_performance_measures.R
+++ 3_performance_measures.R
@@ -1,10 +1,11 @@
+library(speedglm)
 measures <- function(yval, p_val) {
   eta_val <- log(p_val/(1 - p_val))
   # Calibration slope
-  fitcal <- speedglm::speedglm(yval ~ eta_val, family = binomial())
+  fitcal <- speedglm(yval ~ eta_val, family = binomial())
   cal_slope <- as.vector(coef(fitcal)[2])
   # Calibration in the large
-  off <- speedglm::speedglm(yval ~ 1, offset = eta_val, family = binomial())
+  off <- speedglm(yval ~ 1, offset = eta_val, family = binomial())
   cal_large <- as.vector(coef(off))
   # AUC
   cstat <- pROC::roc(response = yval, predictor = as.vector(p_val), levels = c(0, 1), direction = "<")
