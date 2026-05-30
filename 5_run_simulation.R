e82194490706ae01fb1a81d4233c2432d5083804
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-05-30 16:21:47 +0100

    Update 5_run_simulation.R

--- 5_run_simulation.R
+++ 5_run_simulation.R
@@ -1,4 +1,8 @@
 # Register one parallel backend for cv_varsel(parallel = TRUE)
+source("1_sim_parameters.R")
+source("2_data_generation.R")
+source("3_performance_measures.R")
+source("4_projpred_functions.R")
 cl <- parallel::makeCluster(4)
 doParallel::registerDoParallel(cl)
 
