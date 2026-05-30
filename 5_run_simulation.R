b13dff6a15a3487923a6800a90f72343992df0d0
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-05-30 15:34:43 +0100

    Update 5_run_simulation.R

--- 5_run_simulation.R
+++ 5_run_simulation.R
@@ -2,14 +2,12 @@
 cl <- parallel::makeCluster(4)
 doParallel::registerDoParallel(cl)
 
-proj_results <- foreach(
-  i = 1:100,
-  .combine = rbind,
-  .packages = c("rstanarm", "projpred", "pROC", "dplyr"),
-  .export = c("run_projpred_one", "run_one_prior", "generate_ss", "measures")
-) %dopar% {
-  run_projpred_one(i, ndev, n.para, beta0, beta, nval)
-}
+proj_results <- do.call(
+  rbind,
+  lapply(1:10, function(i) {
+    run_projpred_one(i, ndev, n.para, beta0, beta, nval)
+  })
+)
 
 parallel::stopCluster(cl)
 foreach::registerDoSEQ()
