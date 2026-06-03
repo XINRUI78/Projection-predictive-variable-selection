2896ac017d1b0cab3102c51c923deabb8121803b
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-06-03 18:34:45 +0100

    Add files via upload

---
+++ 5_run_la.R
@@ -0,0 +1,39 @@
+source("0_install_packages.R")
+source("1_sim_parameters.R")
+source("2_data_generation.R")
+source("3_performance_measures.R")
+source("4_la.R")
+#source("4_projpred_functions.R")
+
+cl <- parallel::makeCluster(32)
+doParallel::registerDoParallel(cl)
+
+proj_results <- foreach(
+  i = 1:100,
+  .combine = rbind,
+  .packages = c("rstanarm", "projpred", "pROC", "mvtnorm", "speedglm"),
+  .export = c("run_la", "generate_ss", "measures")
+) %dopar% {
+  #run_projpred_one(i, ndev, n.para, beta0, beta, nval)
+  run_la(i, ndev, nval, n.para, beta0, beta)
+}
+
+parallel::stopCluster(cl)
+foreach::registerDoSEQ()
+
+colnames(proj_results) <- c(
+  "prevalence",
+  "anticipated c-stat",
+  "ndev",
+  "method",
+  "calibration slope",
+  "calibration in the large",
+  "auc",
+  "rmspe",
+  paste0("varsel", 1:n.para),
+  "option"
+)
+
+# Save output
+write.csv(proj_results, "proj_results.csv", row.names = FALSE)
+saveRDS(proj_results, "proj_results.rds")
