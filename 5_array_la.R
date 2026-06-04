b8260d4870b23c7f9356d727d9fae3d7adf55e56
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-06-04 16:38:00 +0100

    Add files via upload

---
+++ 5_array_la.R
@@ -0,0 +1,22 @@
+source("0_install_packages.R")
+source("1_sim_parameters.R")
+source("2_data_generation.R")
+source("3_performance_measures.R")
+source("4_la.R")
+
+task_id <- as.integer(Sys.getenv("SGE_TASK_ID"))
+
+proj_results <- run_la(
+  i = task_id,
+  ndev = ndev,
+  nval = nval,
+  n.para = n.para,
+  beta0 = beta0,
+  beta = beta
+)
+
+write.csv(
+  proj_results,
+  sprintf("array_la_%03d.csv", task_id),
+  row.names = FALSE
+)
