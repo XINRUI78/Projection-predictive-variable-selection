2dce8e9dd24467944aba7cb0aa3759624300bd39
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-06-06 12:16:57 +0100

    Update 5_array_nor.R

--- 5_array_nor.R
+++ 5_array_nor.R
@@ -5,7 +5,6 @@ source("3_performance_measures.R")
 source("4_nor.R")
 
 task_id <- as.integer(Sys.getenv("SGE_TASK_ID"))
-run_dir <- Sys.getenv("RUN_DIR")
 
 proj_results <- run_nor(
   i = task_id,
